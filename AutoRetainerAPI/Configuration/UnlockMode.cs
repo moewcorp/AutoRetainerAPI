35713f9c54eeab7d097f62fa6e8abd6525b75591
Limiana <5073202+Limiana@users.noreply.github.com>
2023-10-19 15:38:40 +0300

    Add project files.

---
+++ AutoRetainerAPI/Configuration/UnlockMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRetainerAPI.Configuration
+{
+    [Obfuscation(Exclude = true, ApplyToMembers = true)]
+    public enum UnlockMode
+    {
+        WhileLevelling, 
+        SpamOne, 
+        MultiSelect
+    }
+}
---
+++ AutoRetainerAPI/Configuration/VesselTaskType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRetainerAPI.Configuration
+{
+    [Obfuscation(Exclude = true, ApplyToMembers = true)]
+    public enum VesselTaskType
+    {
+    }
+}
